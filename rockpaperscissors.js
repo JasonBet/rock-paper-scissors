780ce5e0bd40f4032dd79b20c48ef272b30fce9b
Jason <jasonbetsargon@gmail.com>
2024-08-22 06:28:30 -0700

    Create function to generate computer logic

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -6,3 +6,19 @@ Objective 1: write function to get computer choice
     Return "rock", "paper", or "scissors" based on the random number
     Test with console log to ensure this works
 */
+
+function getComputerChoice() {
+    // Generate a random integer (0,1, or 2)
+    let randomNum = Math.floor(Math.random() * 3);
+    console.log(randomNum); 
+    switch (randomNum) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            return "Failed to make a choice";
+    } 
+}
