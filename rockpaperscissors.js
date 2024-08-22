f3716b242f200e58bf7707a271c1b811e2fd48e7
Jason <jasonbetsargon@gmail.com>
2024-08-22 06:17:03 -0700

    Add pseudocode for creating computer logic

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,8 @@
-console.log("Hello universe")
+/* Pseudocode & notes
+Objective 1: write function to get computer choice
+
+    Create function getComputerChoice
+    Generate a random number to determine the computer choice
+    Return "rock", "paper", or "scissors" based on the random number
+    Test with console log to ensure this works
+*/
